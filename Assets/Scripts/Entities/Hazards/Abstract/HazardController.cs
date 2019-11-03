abadc8134c44e7d734bdc46592e617910ddfe208
werignac <48932607+werignac@users.noreply.github.com>
2019-11-03 14:06:51 -0800

    Hazards Class
    There is now a hazards class that the projectile class inherits.

---
+++ Assets/Scripts/Entities/Hazards/Abstract/HazardController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class HazardController : MonoBehaviour
+{
+    public Vector2 moveDirection;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject encounter = collision.gameObject;
+        if (encounter.CompareTag("Shield"))
+        {
+            ShieldController shield = encounter.GetComponentInParent<ShieldController>();
+            shield.Impact(moveDirection);
+
+            OnShieldCollision(encounter);
+        }
+    }
+
+    public void SetMoveDirection(Vector2 dir)
+    {
+        moveDirection = dir;
+    }
+
+    public abstract void OnShieldCollision(GameObject encounter);
+}
---
+++ Assets/Scripts/Entities/Hazards/Projectiles/Abstract/ProjectileController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ProjectileController : HazardController
+{
+    public Vector2 moveDir;
+    private bool rigidBodyControl;
+    private bool hitShield;
+    private Rigidbody2D rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        rigidBodyControl = rigid != null;
+        OnStart();
+    }
+
+    private void Update()
+    {
+        if (!rigidBodyControl)
+        {
+            transform.Translate(moveDirection * Time.deltaTime);
+        }
+        else
+        {
+            SetMoveDirection(rigid.velocity);
+        }
+
+        OnUpdate();
+    }
+
+    public override void OnShieldCollision(GameObject encounter)
+    {
+        float shieldAngle = encounter.GetComponentInParent<Transform>().eulerAngles.z;
+        float projectAngle = Mathf.Atan2(moveDirection.y, moveDirection.x)*Mathf.Rad2Deg;
+        float diff = Mathf.DeltaAngle(projectAngle, shieldAngle);
+        float finalAngle = shieldAngle + diff;
+
+        SetMoveDirection(new Vector2(Mathf.Cos(finalAngle)*moveDirection.magnitude, Mathf.Sin(finalAngle) * moveDirection.magnitude));
+    }
+
+    public abstract void OnStart();
+    public abstract void OnUpdate();
+}
