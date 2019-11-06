5f4be880a6af7b8e0245483c159595f29f0998dc
werignac <48932607+werignac@users.noreply.github.com>
2019-11-05 18:33:03 -0800

    Moving Hazard Class Made
    The projectile has has been further separated into the projectile and moving hazard classes.

--- Assets/Scripts/Entities/Hazards/Projectiles/Abstract/ProjectileController.cs
+++ Assets/Scripts/Entities/Hazards/Projectiles/Abstract/ProjectileController.cs
@@ -5,25 +5,8 @@ using UnityEngine;
 /// <summary>
 /// A class that describes the general behavior of <c>Projectiles</c>.
 /// </summary>
-public abstract class ProjectileController : HazardController
+public abstract class ProjectileController : MovingHazardController
 {
-    /// <summary>
-    /// Instantiates the <c>Projectile</c> (if necessary).
-    /// </summary>
-    private void Start()
-    {
-        OnStart();
-    }
-
-    /// <summary>
-    /// Moves the <c>Projectile</c> every frame.
-    /// </summary>
-    private void Update()
-    {
-        transform.Translate(GetMoveDirection() * Time.deltaTime);
-        OnUpdate();
-    }
-
     /// <summary>
     /// Switches the <c>Projectile</c>'s direction if it encounters a reflecting surface. 
     /// </summary>
@@ -37,13 +20,4 @@ public abstract class ProjectileController : HazardController
 
         SetMoveDirection(new Vector2(Mathf.Cos(finalAngle)* GetMoveDirection().magnitude, Mathf.Sin(finalAngle) * GetMoveDirection().magnitude));
     }
-
-    /// <summary>
-    /// Does something at the beginning of the game.
-    /// </summary>
-    public abstract void OnStart();
-    /// <summary>
-    /// Does something at every frame.
-    /// </summary>
-    public abstract void OnUpdate();
 }
