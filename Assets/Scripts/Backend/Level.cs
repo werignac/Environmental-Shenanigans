9403fd9fa21539fd47ebe7e7b320a74a202626fa
Flameingblast <56851336+Flameingblast@users.noreply.github.com>
2020-01-13 11:18:55 -0800

    VeryBasicProceduralGeneration
    I added the means to create rooms, and the procedural level generation that follows. The Game looks a bit janky, and it's hard to enter good data for the floor placements.

---
+++ Assets/Scripts/Backend/Level.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level
+{
+    Room[] rooms;
+    public Level(int numRooms)
+    {
+        rooms = new Room[numRooms];
+        rooms[0] = Data.GetRoom(0);
+        rooms[numRooms - 1] = Data.GetRoom(1);
+        for(int i = 1; i < numRooms - 1; ++i)
+        {
+            rooms[i] = Data.GetRoom(2);
+        }
+    }
+    public int RoomNum
+    {
+        get { return (rooms.Length); }
+    }
+    public Room GetRoom(int pos)
+    {
+        return (rooms[pos]);
+    }
+}
---
+++ Assets/Scripts/Backend/Room.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room
+{
+    float width;
+    int roomType;
+    ObstacleData[] obstacles;
+    public Room(float w, int r, ObstacleData[] o)
+    {
+        width = w;
+        roomType = r;
+        obstacles = o;
+    }
+    public float Width
+    {
+        get { return (width); }
+    }
+    public int RoomType
+    {
+        get { return (roomType); }
+    }
+    public int ObstacleNum
+    {
+        get { return (obstacles.Length); }
+    }
+    public ObstacleData GetObstacle(int pos)
+    {
+        return (obstacles[pos]);
+    }
+}
