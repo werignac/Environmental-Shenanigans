a8cccd5529677ff6a77455e2cefb3a2d07884017
Flameingblast <56851336+Flameingblast@users.noreply.github.com>
2020-01-14 15:40:30 -0800

    IntroducingProjectileShooters
    I wrote up an abstract class for projectile shooters, and a simple immobile instance of one. Then I created a prefab for it and the simple bullet.

---
+++ Assets/Scripts/Entities/Hazards/ProjectileShooter/ImmobileShooter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImmobileShooter : ProjectileShooterAbstract
+{
+    public string projectileName;
+    public float angle;
+    public float speed;
+
+    public override void Move()
+    {
+        return;
+    }
+
+    public override void ShootProjectile()
+    {
+        GameObject projectile = Instantiate(Resources.Load<GameObject>("Projectiles/" + projectileName), transform.position, new Quaternion());
+        projectile.GetComponent<ProjectileController>().SetMoveDirection(new Vector2(speed * Mathf.Cos((transform.rotation.eulerAngles.x + angle) * Mathf.Deg2Rad), speed * Mathf.Sin((transform.rotation.eulerAngles.x + angle) * Mathf.Deg2Rad)));
+    }
+}
---
+++ Assets/Scripts/Entities/Hazards/ProjectileShooter/ProjectileShooterAbstract.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ProjectileShooterAbstract : MonoBehaviour
+{
+    int count;
+    public int delay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ++count;
+        if(count > delay)
+        {
+            ShootProjectile();
+            count = 0;
+        }
+        Move();
+    }
+
+    public abstract void ShootProjectile();
+    public abstract void Move();
+}
