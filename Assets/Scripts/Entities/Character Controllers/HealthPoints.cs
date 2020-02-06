e08d289925ce99fc8c386dc56f4b5b57e0817139
werignac <48932607+werignac@users.noreply.github.com>
2020-02-05 18:50:49 -0800

    Basic Health
    The player can finally FEEL. In other words I set up a rudamentary system for hitpoints. The same system can work with bosses.

---
+++ Assets/Scripts/Entities/Character Controllers/HealthPoints.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class requires that it be in a game object with children.
+/// Each child of the game object's children will be considered a hitpoint.
+/// </summary>
+public abstract class HealthPoints : MonoBehaviour
+{
+    /// <summary>
+    /// The time between hits when an entity is invisible.
+    /// </summary>
+    public float invincTime;
+    /// <summary>
+    /// The timer that counts how long the entity has been invincible for.
+    /// The timer counts down from invincTime between hits.
+    /// </summary>
+    private float invincTimer;
+    /// <summary>
+    /// The visual hitpoints.
+    /// </summary>
+    private GameObject[] hitPoints;
+    /// <summary>
+    /// The index of the last "available" / "on" hitpoint.
+    /// </summary>
+    private int divider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hitPoints = new GameObject[transform.childCount];
+        int i = 0;
+        foreach (Transform child in transform)
+        {
+            hitPoints[i] = child.gameObject;
+            i++;
+        }
+        divider = hitPoints.Length - 1;
+        invincTimer = invincTime;
+    }
+
+    /// <summary>
+    /// Decrements the invincibility timer every frame.
+    /// </summary>
+    void Update()
+    {
+        if (invincTimer > 0)
+        {
+            invincTimer -= Time.deltaTime;
+            if (invincTimer < 0)
+            {
+                invincTimer = 0;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Removes one hit point. When there are no more hitpoints, NoHealth is called.
+    /// </summary>
+    private void RemoveHitPoint()
+    {
+        hitPoints[divider].SetActive(false);
+        
+        if (divider == 0)
+        {
+            NoHealth();
+        }
+
+        divider--;
+    }
+
+    /// <summary>
+    /// Adds one hit point.
+    /// </summary>
+    /// <returns>Returns whether there was space to add hit points or not.</returns>
+    private bool AddHitPoint()
+    {
+        if (divider != hitPoints.Length - 1)
+        {
+            divider++;
+            hitPoints[divider].SetActive(true);
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Adds multiple hit points.
+    /// </summary>
+    /// <param name="n">The number of hit points to add.</param>
+    private void AddHitPoint(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            AddHitPoint();
+        }
+    }
+
+    /// <summary>
+    /// Removes a hit point if the invincibility timer is out.
+    /// </summary>
+    /// <returns>Whether a hit point was removed or not.</returns>
+    public bool Hit()
+    {
+        if (invincTimer > 0)
+        {
+            return false;
+        }
+        else
+        {
+            invincTimer = invincTime;
+
+            RemoveHitPoint();
+
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Does something when the entity runs out of hit points.
+    /// For example, when the player runs out of hit points,
+    /// the game is reset.
+    /// </summary>
+    public abstract void NoHealth();
+}
---
+++ Assets/Scripts/Entities/Character Controllers/HitArea.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Simple class that checks to see if it was hit by a projectile.
+/// </summary>
+[RequireComponent(typeof(Collider2D))]
+public class HitArea : MonoBehaviour
+{
+    /// <summary>
+    /// The health points associated with the entity.
+    /// </summary>
+    public HealthPoints healthPoints;
+
+    /// <summary>
+    /// Tells the health points when the entity has been hit.
+    /// </summary>
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Projectile"))
+        {
+            healthPoints.Hit();
+        }
+    }
+}
