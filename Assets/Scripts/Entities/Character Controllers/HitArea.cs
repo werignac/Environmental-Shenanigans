87c9f30dc2598fabd0b3ad5624d86bfa305e504e
Flameingblast <56851336+Flameingblast@users.noreply.github.com>
2020-02-07 18:11:47 -0800

    Update HitArea.cs

--- Assets/Scripts/Entities/Character Controllers/HitArea.cs
+++ Assets/Scripts/Entities/Character Controllers/HitArea.cs
@@ -18,7 +18,7 @@ public class HitArea : MonoBehaviour
     /// </summary>
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Projectile"))
+        if (other.gameObject.CompareTag("Projectile") && healthPoints != null)
         {
             healthPoints.Hit();
         }
