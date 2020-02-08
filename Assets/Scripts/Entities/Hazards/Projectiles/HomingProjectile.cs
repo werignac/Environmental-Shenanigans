3a870bc8ef1881b9f3a0379151275ca4096992f2
werignac <werignac@comcast.net>
2020-02-07 16:53:26 -0800

    Homing projectiles
    Homing projectiles and other changes...

---
+++ Assets/Scripts/Entities/Hazards/Projectiles/HomingProjectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingProjectile : AcceleratingProjectileController
+{
+    private float accelerationMag;
+    public Transform target;
+    public float targetDist;
+
+    public override void OnStart()
+    {
+        accelerationMag = acceleration.magnitude;
+    }
+
+    public override void OnUpdate()
+    {
+        if (getDistance() < targetDist)
+        {
+            Vector2 difference = target.position - transform.position;
+            float ratio = accelerationMag / difference.magnitude;
+            acceleration = new Vector2(ratio * difference.x, ratio * difference.y);
+        }
+
+        
+
+        base.OnUpdate();
+    }
+}
