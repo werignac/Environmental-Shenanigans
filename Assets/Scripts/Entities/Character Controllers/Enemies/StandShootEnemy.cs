c9fa0bf30403a95755ccaa61fd185cde8c45cc6f
Flameingblast <56851336+Flameingblast@users.noreply.github.com>
2020-02-08 16:35:04 -0800

    RangeEnemyFix
    I made it so ranged enemies shoot correctly, and made a level featuring them.

--- Assets/Scripts/Entities/Character Controllers/Enemies/StandShootEnemy.cs
+++ Assets/Scripts/Entities/Character Controllers/Enemies/StandShootEnemy.cs
@@ -13,16 +13,22 @@ public class StandShootEnemy : Enemy
     public override void OnStart()
     {
         base.OnStart();
-        count = delay;
+        count = (int)(delay * Data.frameRate);
     }
     public override void Move()
     {
+        --count;
         if(count <= 0)
         {
             GameObject projectile = Instantiate(Resources.Load<GameObject>("Projectiles/" + projectileName), transform.position, new Quaternion());
-            float angle = Mathf.Atan((Data.playerPos.y - transform.position.y) / (Data.playerPos.x - transform.position.x));
+            float angle = Mathf.Rad2Deg * Mathf.Atan((Data.playerPos.y - transform.position.y) / (Data.playerPos.x - transform.position.x));
+            if(Data.playerPos.x < transform.position.x)
+            {
+                angle += 180;
+            }
             projectile.GetComponent<ProjectileController>().SetMoveDirection(new Vector2(speed * Mathf.Cos((transform.rotation.eulerAngles.x + angle) * Mathf.Deg2Rad), speed * Mathf.Sin((transform.rotation.eulerAngles.x + angle) * Mathf.Deg2Rad)));
             projectile.GetComponent<ProjectileController>().range = range;
+            count = (int)(delay * Data.frameRate);
         }
     }
 }
