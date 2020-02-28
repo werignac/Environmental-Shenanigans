425e46f80bf319029b72c6ffdbb89434a4d747aa
Flameingblast <56851336+Flameingblast@users.noreply.github.com>
2020-02-27 17:22:38 -0800

    AnimationTriggerAddedRoomFinished

--- Assets/Scripts/Entities/Character Controllers/Enemies/StandShootEnemy.cs
+++ Assets/Scripts/Entities/Character Controllers/Enemies/StandShootEnemy.cs
@@ -9,21 +9,27 @@ public class StandShootEnemy : Enemy
     public string projectileName;
     public int speed;
     public int range;
-    private int count;
+    private float count;
     public AudioSource sFXPlayer;
+    private Animator animator;
 
     public override void OnStart()
     {
         base.OnStart();
-        count = (int)(delay * Data.frameRate) - (int)(initialCount * Data.frameRate);
+        count = initialCount;
+        animator = GetComponent<Animator>();
     }
     public override void Move()
     {
-        --count;
-        if(count <= 0)
+        count += Time.deltaTime;
+        if(count >= delay)
         {
             sFXPlayer.clip = Resources.Load<AudioClip>("Sounds/SpearThrow");
             sFXPlayer.Play();
+            if(animator != null)
+            {
+                animator.SetTrigger("Fire");
+            }
             GameObject projectile = Instantiate(Resources.Load<GameObject>("Projectiles/" + projectileName), transform.position, new Quaternion());
             float angle = Mathf.Rad2Deg * Mathf.Atan((Data.playerPos.y - transform.position.y) / (Data.playerPos.x - transform.position.x));
             if(Data.playerPos.x < transform.position.x)
@@ -32,7 +38,7 @@ public class StandShootEnemy : Enemy
             }
             projectile.GetComponent<ProjectileController>().SetMoveDirection(new Vector2(speed * Mathf.Cos((transform.rotation.eulerAngles.x + angle) * Mathf.Deg2Rad), speed * Mathf.Sin((transform.rotation.eulerAngles.x + angle) * Mathf.Deg2Rad)));
             projectile.GetComponent<ProjectileController>().range = range;
-            count = (int)(delay * Data.frameRate);
+            count = 0;
         }
     }
 }
