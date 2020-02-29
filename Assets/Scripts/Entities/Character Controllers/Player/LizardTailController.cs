c670c7eecb6e876335f9706d1cf2ba17c1a515f6
werignac <werignac@comcast.net>
2020-02-28 20:30:36 -0800

    Lizard Tail

--- Assets/Scripts/Entities/Character Controllers/Player/LizardTailController.cs
+++ Assets/Scripts/Entities/Character Controllers/Player/LizardTailController.cs
@@ -8,14 +8,14 @@ public class LizardTailController : MonoBehaviour
     public Transform copyRot;
     public float offset;
     public float radius;
-    public float flip = 1;
+    public float innerMultiply;
 
     // Start is called before the first frame update
     void Start()
     {
         segments = new List<GameObject>();
-        GameObject segment = transform.GetChild(0).gameObject;
-        while (segment != null)
+        GameObject segment = gameObject;
+        while (segment.transform.childCount > 0)
         {
             segments.Add(segment);
             segment = segment.transform.GetChild(0).gameObject;
@@ -24,9 +24,17 @@ public class LizardTailController : MonoBehaviour
 
     private void GetCurve(float radius, float theta)
     {
+        bool isNegative = theta > Mathf.PI;
+        if (isNegative)
+        {
+            theta = theta - 2 * (theta - Mathf.PI);
+        }
+
         float innerCoef = Mathf.Pow(0.460421f, theta + 0.827743f) + 0.368267f;
+        innerCoef *= innerMultiply;
         float outerCoef = radius / (1 + Mathf.Cos(innerCoef * theta));
         float maxAngle = Mathf.Acos(-1) / innerCoef;
+
         float lastAngle = maxAngle;
 
         float angleDivisions = maxAngle / segments.Count;
@@ -36,13 +44,23 @@ public class LizardTailController : MonoBehaviour
 
             SpriteRenderer sprite = segment.GetComponent<SpriteRenderer>();
 
-            float length = sprite.size.y*segment.transform.lossyScale.y;
-
-            lastAngle = Mathf.Acos(length / outerCoef - 1) / innerCoef; //- lastAngle;
-
-            segment.transform.localRotation = Quaternion.Euler(0, 0, lastAngle*Mathf.Rad2Deg*flip + offset);
+            float instantRadius = outerCoef * (1 - Mathf.Cos(innerCoef * lastAngle));
+            Vector2 funcPos = new Vector2(instantRadius * Mathf.Cos(lastAngle), instantRadius * Mathf.Sin(lastAngle));
 
             lastAngle -= angleDivisions;
+
+            instantRadius = outerCoef * (1 - Mathf.Cos(innerCoef * lastAngle));
+            Vector2 nextFuncPos = new Vector2(instantRadius * Mathf.Cos(lastAngle), instantRadius * Mathf.Sin(lastAngle));
+
+            Vector2 difference = funcPos - nextFuncPos;
+
+            float slopeAngle = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+
+            if (isNegative)
+            {
+                slopeAngle *= -1;
+            }
+            segment.transform.rotation = Quaternion.Euler(0, 0, slopeAngle + offset);
         }
     }
 
