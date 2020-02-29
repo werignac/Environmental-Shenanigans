42e1515f749d0a57209bddae84ae71616f6abe12
Flameingblast <56851336+Flameingblast@users.noreply.github.com>
2020-02-29 15:21:50 -0800

    FixGlassShard&&BossDamageNotExisting

--- Assets/Scripts/Entities/Character Controllers/Enemies/EnemyProjectileDamage.cs
+++ Assets/Scripts/Entities/Character Controllers/Enemies/EnemyProjectileDamage.cs
@@ -14,7 +14,10 @@ public class EnemyProjectileDamage : MonoBehaviour
         {
             Destroy(other.gameObject);
             enemy.Damage();
-            bossPoints.RemoveHitPoint();
+            if (bossPoints != null)
+            {
+                bossPoints.RemoveHitPoint();
+            }
         }
     }
 }
