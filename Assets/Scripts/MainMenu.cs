7eea6eef9c4b234c81c3fe2b65b3e0575f254d54
Flameingblast <56851336+Flameingblast@users.noreply.github.com>
2020-03-01 12:11:56 -0800

    BossWork
    I reworked the bosses a little, and made the car prefab in obstacles actually change the car in the room.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Data.rooms = 15;
+        Data.rooms = 2;
     }
 
     // Update is called once per frame
