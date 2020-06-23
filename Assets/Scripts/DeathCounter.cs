c56186f3e1d129e595de706fa2159bfdac7c5423
Flameingblast <56851336+Flameingblast@users.noreply.github.com>
2020-06-23 15:49:30 -0700

    MadeDeathCounter
    Implemented a counter to the game, so you can see how many times you've died.

---
+++ Assets/Scripts/DeathCounter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathCounter : MonoBehaviour
+{
+    TextMesh display;
+    // Start is called before the first frame update
+    void Start()
+    {
+        display = GetComponent<TextMesh>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        display.text = "" + Data.currentDeaths;
+    }
+}
