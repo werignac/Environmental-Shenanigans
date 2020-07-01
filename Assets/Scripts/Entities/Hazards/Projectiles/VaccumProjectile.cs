97bd1467966b0fd78e6417e42861cb8cf28c4568
Flameingblast <56851336+Flameingblast@users.noreply.github.com>
2020-07-01 10:37:59 -0700

    VaccumProjectileScripts
    Made the scripts to attach to vaccum projectiles.

---
+++ Assets/Scripts/Entities/Hazards/Projectiles/VaccumProjectile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VaccumProjectile : ConstantProjectile
+{
+    public override void OnShieldCollision(GameObject encounter)
+    {
+        base.OnShieldCollision(encounter);
+        SetMoveDirection(new Vector2(GetMoveDirection().x * -1, GetMoveDirection().y * -1));
+    }
+}
